3446c2556917cbab68536753059c4bd3510ce432
fulcrum-ctrl <71572528+fulcrum-ctrl@users.noreply.github.com>
2020-11-03 16:28:38 -0500

    2nd commit. Updated index and script.

--- script.js
+++ script.js
@@ -17,25 +17,91 @@ console.log("toISOString: " + now.toISOString());
 function update(){
     $('#currentDay').html(moment().format('D. MMMM YYYY H:mm:ss'));
 }
-
 setInterval(update, 1000);
 
 //Create rows columns as page loads
 //should I be appending to .row or .container?
-$(document).ready(function(){
-    //var businessHours = ["9 AM","10 AM", "11 AM", "12PM"];
-    for (count=0;count<8; count++){
-        var sideBar = $("<div>")
-        sideBar.addClass("col-sm-2 idkYet")
-        var toDoList = $("<div>");
-        var row = $("<div>");
-        row.addClass("row");
-        toDoList.addClass("col-sm-8 toDo");
-        var saveButton = $("<button>");
-        saveButton.addClass("saveBtn");
-        $(".container").append(row);
-        $(".row").append(sideBar);
-        $(".row").append(toDoList);
-        $(".row").append(saveButton);
-    }
-})
+//Now got to assign different ID's to every button
+//but why tho
+
+
+//Review for tonight:
+
+//var saveOne = document.getElementById("save1");
+//console.log(saveOne);
+//Why is this returning null?
+//saveOne.addEventListener("click",function(){
+//    alert("Sir, I was pressed.");
+//})
+//Create save button functionality
+//Store and callback
+//if condition for color scheme re: past, present, future
+//may have to store moment element in var (continuous saving?)
+//How to make content editable and storable
+
+//SOLUTION: HARD CODE ALL ELEMENTS
+
+var saveOne = document.getElementById("buttonOne");
+saveOne.addEventListener("click",function(){
+    var tabOne = document.getElementById("toDoOne");
+    var storeOne = tabOne.textContent;
+    console.log("The button works and: " + storeOne);
+    localStorage.setItem("1",storeOne);
+    alert("Item Saved!");
+});
+var saveTwo = document.getElementById("buttonTwo");
+saveTwo.addEventListener("click",function(){
+    var tabTwo = document.getElementById("toDoTwo");
+    var storeTwo = tabTwo.textContent;
+    console.log("The button works and: " + storeTwo);
+    localStorage.setItem("2",storeTwo);
+    alert("Item Saved!");
+});
+var saveThree = document.getElementById("buttonThree");
+saveThree.addEventListener("click",function(){
+    var tabThree = document.getElementById("toDoThree");
+    var storeThree = tabThree.textContent;
+    console.log("The button works and: " + storeThree);
+    localStorage.setItem("3",storeThree);
+    alert("Item Saved!");
+});
+var saveFour = document.getElementById("buttonFour");
+saveFour.addEventListener("click",function(){
+    var tabFour = document.getElementById("toDoFour");
+    var storeFour = tabFour.textContent;
+    console.log("The button works and: " + storeFour);
+    localStorage.setItem("4",storeFour);
+    alert("Item Saved!");
+});
+var saveFive = document.getElementById("buttonFive");
+saveOne.addEventListener("click",function(){
+    var tabFive = document.getElementById("toDoFive");
+    var storeFive = tabFive.textContent;
+    console.log("The button works and: " + storeFive);
+    localStorage.setItem("5",storeFive);
+    alert("Item Saved!");
+});
+var saveSix = document.getElementById("buttonSix");
+saveSix.addEventListener("click",function(){
+    var tabSix = document.getElementById("toDoSix");
+    var storeSix = tabSix.textContent;
+    console.log("The button works and: " + storeSix);
+    localStorage.setItem("6",storeSix);
+    alert("Item Saved!");
+});
+var saveSeven = document.getElementById("buttonSeven");
+saveSeven.addEventListener("click",function(){
+    var tabSeven = document.getElementById("toDoSeven");
+    var storeSeven = tabSeven.textContent;
+    console.log("The button works and: " + storeSeven);
+    localStorage.setItem("7",storeSeven);
+    alert("Item Saved!");
+});
+var saveEight = document.getElementById("buttonEight");
+saveEight.addEventListener("click",function(){
+    var tabEight = document.getElementById("toDoSix");
+    var storeEight = tabEight.textContent;
+    console.log("The button works and: " + storeEight);
+    localStorage.setItem("8",storeEight);
+    alert("Item Saved!");
+});
